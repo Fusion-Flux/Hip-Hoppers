1d80f42b9a4727d17c17ce740efb3f3fe4250158
Fusion Flux <c4gamer2.0@gmail.com>
2022-04-17 11:27:59 -0500

    added crafting recipes

--- src/main/java/com/fusionflux/hip_hoppers/HipHoppersMod.java
+++ src/main/java/com/fusionflux/hip_hoppers/HipHoppersMod.java
@@ -2,6 +2,10 @@ package com.fusionflux.hip_hoppers;
 
 import net.fabricmc.api.ModInitializer;
 import com.fusionflux.hip_hoppers.blocks.HipHoppersBlocks;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +14,11 @@ public class HipHoppersMod implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("hip_hoppers");
+
+	public static final ItemGroup HipHoppersGroup = FabricItemGroupBuilder.create(
+					new Identifier("hip_hoppers", "other"))
+			.icon(() -> new ItemStack(HipHoppersBlocks.UPPER_BLOCK))
+			.build();
 
 	@Override
 	public void onInitialize() {
--- src/main/java/com/fusionflux/hip_hoppers/blocks/HipHoppersBlocks.java
+++ src/main/java/com/fusionflux/hip_hoppers/blocks/HipHoppersBlocks.java
@@ -1,5 +1,6 @@
 package com.fusionflux.hip_hoppers.blocks;
 
+import com.fusionflux.hip_hoppers.HipHoppersMod;
 import com.fusionflux.hip_hoppers.blocks.DownHoppers.EnderHopperBlock;
 import com.fusionflux.hip_hoppers.blocks.DownHoppers.EnderHopperBlockEntity;
 import com.fusionflux.hip_hoppers.blocks.NormalHoppers.CustomHopperBlock;
@@ -20,68 +21,66 @@ import net.minecraft.util.registry.Registry;
 
 public class HipHoppersBlocks {
     public static final UpperBlock UPPER_BLOCK = new UpperBlock(FabricBlockSettings.copyOf(Blocks.HOPPER),8);
-    public static final UpperBlock SPEEDY_UPPER_BLOCK = new UpperBlock(FabricBlockSettings.copyOf(Blocks.HOPPER),4);
-    public static final UpperBlock SUPER_SPEEDY_UPPER_BLOCK = new UpperBlock(FabricBlockSettings.copyOf(Blocks.HOPPER),2);
+    public static final UpperBlock COPPER_UPPER_BLOCK = new UpperBlock(FabricBlockSettings.copyOf(Blocks.HOPPER),4);
+    public static final UpperBlock GOLD_PLATED_COPPER_UPPER_BLOCK = new UpperBlock(FabricBlockSettings.copyOf(Blocks.HOPPER),2);
     public static BlockEntityType<UpperBlockEntity> UPPER_BLOCK_ENTITY;
 
     public static final EnderUpperBlock ENDER_UPPER_BLOCK = new EnderUpperBlock(FabricBlockSettings.copyOf(Blocks.HOPPER),8);
-    public static final EnderUpperBlock SPEEDY_ENDER_UPPER_BLOCK = new EnderUpperBlock(FabricBlockSettings.copyOf(Blocks.HOPPER),4);
-    public static final EnderUpperBlock SUPER_SPEEDY_ENDER_UPPER_BLOCK = new EnderUpperBlock(FabricBlockSettings.copyOf(Blocks.HOPPER),2);
+    public static final EnderUpperBlock COPPER_ENDER_UPPER_BLOCK = new EnderUpperBlock(FabricBlockSettings.copyOf(Blocks.HOPPER),4);
+    public static final EnderUpperBlock GOLD_PLATED_COPPER_ENDER_UPPER_BLOCK = new EnderUpperBlock(FabricBlockSettings.copyOf(Blocks.HOPPER),2);
     public static BlockEntityType<EnderUpperBlockEntity> ENDER_UPPER_BLOCK_ENTITY;
 
     public static final EnderHopperBlock ENDER_HOPPER_BLOCK = new EnderHopperBlock(FabricBlockSettings.copyOf(Blocks.HOPPER),8);
-    public static final EnderHopperBlock SPEEDY_ENDER_HOPPER_BLOCK = new EnderHopperBlock(FabricBlockSettings.copyOf(Blocks.HOPPER),4);
-    public static final EnderHopperBlock SUPER_SPEEDY_ENDER_HOPPER_BLOCK = new EnderHopperBlock(FabricBlockSettings.copyOf(Blocks.HOPPER),2);
+    public static final EnderHopperBlock COPPER_ENDER_HOPPER_BLOCK = new EnderHopperBlock(FabricBlockSettings.copyOf(Blocks.HOPPER),4);
+    public static final EnderHopperBlock GOLD_PLATED_COPPER_ENDER_HOPPER_BLOCK = new EnderHopperBlock(FabricBlockSettings.copyOf(Blocks.HOPPER),2);
     public static BlockEntityType<EnderHopperBlockEntity> ENDER_HOPPER_BLOCK_ENTITY;
 
 
-    public static final CustomHopperBlock SPEEDY_HOPPER_BLOCK = new CustomHopperBlock(FabricBlockSettings.copyOf(Blocks.HOPPER),4);
-    public static final CustomHopperBlock SUPER_SPEEDY_HOPPER_BLOCK = new CustomHopperBlock(FabricBlockSettings.copyOf(Blocks.HOPPER),2);
+    public static final CustomHopperBlock COPPER_HOPPER_BLOCK = new CustomHopperBlock(FabricBlockSettings.copyOf(Blocks.HOPPER),4);
+    public static final CustomHopperBlock GOLD_PLATED_COPPER_HOPPER_BLOCK = new CustomHopperBlock(FabricBlockSettings.copyOf(Blocks.HOPPER),2);
     public static BlockEntityType<CustomHopperBlockEntity> CUSTOM_HOPPER_BLOCK_ENTITY;
 
 
     public static void registerBlocks() {
-        UPPER_BLOCK_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, new Identifier("hip_hoppers", "upper_entity"), FabricBlockEntityTypeBuilder.create(UpperBlockEntity::new, UPPER_BLOCK,SPEEDY_UPPER_BLOCK,SUPER_SPEEDY_UPPER_BLOCK).build(null));
-        Registry.register(Registry.BLOCK, new Identifier("hip_hoppers", "upper"), UPPER_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("hip_hoppers", "upper"), new BlockItem(UPPER_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE)));
 
-        Registry.register(Registry.BLOCK, new Identifier("hip_hoppers", "speedy_upper"), SPEEDY_UPPER_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("hip_hoppers", "speedy_upper"), new BlockItem(SPEEDY_UPPER_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE)));
-
-        Registry.register(Registry.BLOCK, new Identifier("hip_hoppers", "super_speedy_upper"), SUPER_SPEEDY_UPPER_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("hip_hoppers", "super_speedy_upper"), new BlockItem(SUPER_SPEEDY_UPPER_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE)));
-
-
-        ENDER_UPPER_BLOCK_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, new Identifier("hip_hoppers", "ender_upper_entity"), FabricBlockEntityTypeBuilder.create(EnderUpperBlockEntity::new, ENDER_UPPER_BLOCK,SPEEDY_ENDER_UPPER_BLOCK,SUPER_SPEEDY_ENDER_UPPER_BLOCK).build(null));
-        Registry.register(Registry.BLOCK, new Identifier("hip_hoppers", "ender_upper"), ENDER_UPPER_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("hip_hoppers", "ender_upper"), new BlockItem(ENDER_UPPER_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE)));
-
-        Registry.register(Registry.BLOCK, new Identifier("hip_hoppers", "speedy_ender_upper"), SPEEDY_ENDER_UPPER_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("hip_hoppers", "speedy_ender_upper"), new BlockItem(SPEEDY_ENDER_UPPER_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE)));
-
-        Registry.register(Registry.BLOCK, new Identifier("hip_hoppers", "super_speedy_ender_upper"), SUPER_SPEEDY_ENDER_UPPER_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("hip_hoppers", "super_speedy_ender_upper"), new BlockItem(SUPER_SPEEDY_ENDER_UPPER_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE)));
-
-
-        ENDER_HOPPER_BLOCK_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, new Identifier("hip_hoppers", "ender_hopper_entity"), FabricBlockEntityTypeBuilder.create(EnderHopperBlockEntity::new, ENDER_HOPPER_BLOCK, SPEEDY_ENDER_HOPPER_BLOCK, SUPER_SPEEDY_ENDER_HOPPER_BLOCK).build(null));
+        ENDER_HOPPER_BLOCK_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, new Identifier("hip_hoppers", "ender_hopper_entity"), FabricBlockEntityTypeBuilder.create(EnderHopperBlockEntity::new, ENDER_HOPPER_BLOCK, COPPER_ENDER_HOPPER_BLOCK, GOLD_PLATED_COPPER_ENDER_HOPPER_BLOCK).build(null));
         Registry.register(Registry.BLOCK, new Identifier("hip_hoppers", "ender_hopper"), ENDER_HOPPER_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("hip_hoppers", "ender_hopper"), new BlockItem(ENDER_HOPPER_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE)));
+        Registry.register(Registry.ITEM, new Identifier("hip_hoppers", "ender_hopper"), new BlockItem(ENDER_HOPPER_BLOCK, new Item.Settings().group(HipHoppersMod.HipHoppersGroup)));
 
-        Registry.register(Registry.BLOCK, new Identifier("hip_hoppers", "speedy_ender_hopper"), SPEEDY_ENDER_HOPPER_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("hip_hoppers", "speedy_ender_hopper"), new BlockItem(SPEEDY_ENDER_HOPPER_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE)));
+        Registry.register(Registry.BLOCK, new Identifier("hip_hoppers", "copper_ender_hopper"), COPPER_ENDER_HOPPER_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier("hip_hoppers", "copper_ender_hopper"), new BlockItem(COPPER_ENDER_HOPPER_BLOCK, new Item.Settings().group(HipHoppersMod.HipHoppersGroup)));
 
-        Registry.register(Registry.BLOCK, new Identifier("hip_hoppers", "super_speedy_ender_hopper"), SUPER_SPEEDY_ENDER_HOPPER_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("hip_hoppers", "super_speedy_ender_hopper"), new BlockItem(SUPER_SPEEDY_ENDER_HOPPER_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE)));
+        Registry.register(Registry.BLOCK, new Identifier("hip_hoppers", "gold_plated_copper_ender_hopper"), GOLD_PLATED_COPPER_ENDER_HOPPER_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier("hip_hoppers", "gold_plated_copper_ender_hopper"), new BlockItem(GOLD_PLATED_COPPER_ENDER_HOPPER_BLOCK, new Item.Settings().group(HipHoppersMod.HipHoppersGroup)));
+
+        CUSTOM_HOPPER_BLOCK_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, new Identifier("hip_hoppers", "custom_hopper_entity"), FabricBlockEntityTypeBuilder.create(CustomHopperBlockEntity::new,COPPER_HOPPER_BLOCK, GOLD_PLATED_COPPER_HOPPER_BLOCK).build(null));
+        Registry.register(Registry.BLOCK, new Identifier("hip_hoppers", "copper_hopper"), COPPER_HOPPER_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier("hip_hoppers", "copper_hopper"), new BlockItem(COPPER_HOPPER_BLOCK, new Item.Settings().group(HipHoppersMod.HipHoppersGroup)));
+
+        Registry.register(Registry.BLOCK, new Identifier("hip_hoppers", "gold_plated_copper_hopper"), GOLD_PLATED_COPPER_HOPPER_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier("hip_hoppers", "gold_plated_copper_hopper"), new BlockItem(GOLD_PLATED_COPPER_HOPPER_BLOCK, new Item.Settings().group(HipHoppersMod.HipHoppersGroup)));
 
 
 
+        UPPER_BLOCK_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, new Identifier("hip_hoppers", "upper_entity"), FabricBlockEntityTypeBuilder.create(UpperBlockEntity::new, UPPER_BLOCK,COPPER_UPPER_BLOCK,GOLD_PLATED_COPPER_UPPER_BLOCK).build(null));
+        Registry.register(Registry.BLOCK, new Identifier("hip_hoppers", "upper"), UPPER_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier("hip_hoppers", "upper"), new BlockItem(UPPER_BLOCK, new Item.Settings().group(HipHoppersMod.HipHoppersGroup)));
 
+        Registry.register(Registry.BLOCK, new Identifier("hip_hoppers", "copper_upper"), COPPER_UPPER_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier("hip_hoppers", "copper_upper"), new BlockItem(COPPER_UPPER_BLOCK, new Item.Settings().group(HipHoppersMod.HipHoppersGroup)));
 
-        CUSTOM_HOPPER_BLOCK_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, new Identifier("hip_hoppers", "custom_hopper_entity"), FabricBlockEntityTypeBuilder.create(CustomHopperBlockEntity::new,SPEEDY_HOPPER_BLOCK, SUPER_SPEEDY_HOPPER_BLOCK).build(null));
-        Registry.register(Registry.BLOCK, new Identifier("hip_hoppers", "speedy_hopper"), SPEEDY_HOPPER_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("hip_hoppers", "speedy_hopper"), new BlockItem(SPEEDY_HOPPER_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE)));
+        Registry.register(Registry.BLOCK, new Identifier("hip_hoppers", "gold_plated_copper_upper"), GOLD_PLATED_COPPER_UPPER_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier("hip_hoppers", "gold_plated_copper_upper"), new BlockItem(GOLD_PLATED_COPPER_UPPER_BLOCK, new Item.Settings().group(HipHoppersMod.HipHoppersGroup)));
 
-        Registry.register(Registry.BLOCK, new Identifier("hip_hoppers", "super_speedy_hopper"), SUPER_SPEEDY_HOPPER_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("hip_hoppers", "super_speedy_hopper"), new BlockItem(SUPER_SPEEDY_HOPPER_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE)));
+        ENDER_UPPER_BLOCK_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, new Identifier("hip_hoppers", "ender_upper_entity"), FabricBlockEntityTypeBuilder.create(EnderUpperBlockEntity::new, ENDER_UPPER_BLOCK,COPPER_ENDER_UPPER_BLOCK,GOLD_PLATED_COPPER_ENDER_UPPER_BLOCK).build(null));
+        Registry.register(Registry.BLOCK, new Identifier("hip_hoppers", "ender_upper"), ENDER_UPPER_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier("hip_hoppers", "ender_upper"), new BlockItem(ENDER_UPPER_BLOCK, new Item.Settings().group(HipHoppersMod.HipHoppersGroup)));
+
+        Registry.register(Registry.BLOCK, new Identifier("hip_hoppers", "copper_ender_upper"), COPPER_ENDER_UPPER_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier("hip_hoppers", "copper_ender_upper"), new BlockItem(COPPER_ENDER_UPPER_BLOCK, new Item.Settings().group(HipHoppersMod.HipHoppersGroup)));
+
+        Registry.register(Registry.BLOCK, new Identifier("hip_hoppers", "gold_plated_copper_ender_upper"), GOLD_PLATED_COPPER_ENDER_UPPER_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier("hip_hoppers", "gold_plated_copper_ender_upper"), new BlockItem(GOLD_PLATED_COPPER_ENDER_UPPER_BLOCK, new Item.Settings().group(HipHoppersMod.HipHoppersGroup)));
+
     }
 }
